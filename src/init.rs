c447ba6f96705475d6a3d0e5c381c58b91ea3bca
Thomas Campistron <irevoire@hotmail.fr>
2019-04-07 01:35:08 +0200

    add clap dependency to parse the command line args. Now we can also define the server addr and our name on the command line or by the environment

--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,40 @@
+use clap::{App, Arg};
+use env_logger::Env;
 use log::info;
 use std::collections::HashMap;
 
-pub fn init(name: &str) -> String {
+pub struct Args {
+    pub addr: String,
+    pub cookie: String,
+}
+
+pub fn init() -> Args {
+    kankyo::load().unwrap_or_else(|e| println!("No env file: {}", e));
+    env_logger::from_env(Env::default().default_filter_or("rustberrypi")).init();
+
+    let matches = App::new("rustberrypy")
+        .author("irevoire <irevoire.ovh>")
+        .about("Monitor raspberry pi")
+        .arg(
+            Arg::with_name("name")
+                .short("n")
+                .long("name")
+                .env("NAME")
+                .default_value("pi") // TODO hostname
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("server")
+                .short("s")
+                .long("server")
+                .env("SERVER")
+                .default_value("localhost:3000")
+                .takes_value(true),
+        )
+        .get_matches();
+    let name = matches.value_of("name").unwrap();
+    let server = matches.value_of("server").unwrap();
+
     let mut param = HashMap::new();
 
     param.insert("name", name);
@@ -18,5 +51,8 @@ pub fn init(name: &str) -> String {
     let cookie = res.headers().get("set-cookie").unwrap();
     let cookie = cookie.to_str().unwrap().split(";").next().unwrap();
 
-    return cookie.to_string();
+    return Args {
+        cookie: cookie.to_string(),
+        addr: server.to_string(),
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
-use std::{thread, time::Duration};
-
-use env_logger::Env;
-use log::info;
-
 use clokwerk::Scheduler;
+use std::{thread, time::Duration};
 
 mod init;
 mod modules;
 
 fn main() {
-    kankyo::load().unwrap_or_else(|e| println!("No env file: {}", e));
-    env_logger::from_env(Env::default().default_filter_or("rustberrypi")).init();
+    let args = init::init();
 
     let mut scheduler = Scheduler::new();
 
-    let cookie = init::init("bloubi");
-    info!("Got the cookie: {}", cookie);
-
-    modules::cpu_temp::init(&mut scheduler, &cookie);
-    modules::uptime::init(&mut scheduler, &cookie);
+    modules::cpu_temp::init(&mut scheduler, &args.cookie);
+    modules::uptime::init(&mut scheduler, &args.cookie);
 
     loop {
         scheduler.run_pending();
